b2132037981089f0d174effac2555604d2950705
mahishree949 <mahisrimahi96@gmail.com>
2025-07-31 14:32:00 +0530

    Create input.py
    integer

---
+++ input.py
@@ -0,0 +1,7 @@
+Input calculation.py
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Program Files/Python312/Input calculation.py
+Enter a number7
+ODD
