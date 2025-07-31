58b181b6f73fefd5b9502011f99f09d3cfed2765
mahishree949 <mahisrimahi96@gmail.com>
2025-07-31 14:55:14 +0530

    Create Algorithms.py
    Algorithms

---
+++ Algorithms.py
@@ -0,0 +1,9 @@
+Algorithms.txt
+Odd Or Even
+Algorithm:
+Step 1 : Start the program
+Step 2 : Read the value a
+Step 3 : if a%2==0 then
+Step 4 : print the value is even
+Step 5 : Otherwise the value is odd
+Step 6 : stop the execution
