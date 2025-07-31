256b6223bcf38da1720c00588e160cabd782d0fb
mahishree949 <mahisrimahi96@gmail.com>
2025-07-31 14:35:36 +0530

    Create greatest.py
    greatest

---
+++ greatest.py
@@ -0,0 +1,9 @@
+Greatest.py
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Program Files/Python312/Greatest.py
+Enter  Number 198
+Enter  Number 250
+Enter  Number 370
+A is a greatest
