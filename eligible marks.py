8f6a860350375edace38657fa9e40d392300d500
mahishree949 <mahisrimahi96@gmail.com>
2025-07-31 14:40:22 +0530

    Create eligible marks.py
    marks

---
+++ eligible marks.py
@@ -0,0 +1,7 @@
+eligible marks.py
+Python 3.12.3 (tags/v3.12.3:f6650f9, Apr  9 2024, 14:05:25) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Program Files/Python312/eligible marks.py
+Enter a mark90
+A grade
